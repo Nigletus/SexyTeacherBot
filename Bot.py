785c31fa28e7c60478d0e45e4bd865ff9584de7d
ghostahull <ghostahull@protonmail.com>
2017-05-30 22:43:52 +0100

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -27,7 +27,7 @@ class Bot:
             exit()
 
     def _send(self, msg):
-        self.s.send(msg.encode())
+        self.s.send(msg.encode("UTF-8"))
 
     def message(self, msg, chan):
         self._send("PRIVMSG %s :%s\r\n" % (chan, msg))
@@ -50,6 +50,7 @@ class Bot:
                 if "PING" in recvd:
                     self.pong(recvd)
                 elif "%s!%s" % (self.conf["nick"], self.conf["user"]) in recvd:
+                    print("[+] Ping completed")
                     break
 
             except socket.timeout:
@@ -59,21 +60,25 @@ class Bot:
         num = msg.strip("PING :")
         self._send("PONG :%s" % num)
 
+    def login(self):
+        self._send("PRIVMSG nickserv :identify %s\r\n" % self.conf["pass"])
+
     def join(self):
         print("[+] Joining channels.\n")
-        self._send(":source PRIVMSG nickserv :IDENTIFY %s\r\n" % self.conf["pass"])
+
+        [self.login() for _ in range(3)]
 
         for x in self.conf["chans"]:
-            self._send(":source JOIN :%s\r\n" % x)
+            self._send("JOIN %s\r\n" % x)
 
-        self._send(":source MODE %s +B \r\n" % self.conf["nick"])
+        self._send("MODE %s +B\r\n" % self.conf["nick"])
 
     def listen(self):
         try:
             recvd = self.s.recv(1024).decode()
         except socket.timeout:
             print("[-] Error: Socket timeout.")
-            return self.listen()
+            self.listen()
 
         info = recvd.split()[3:]
         msg = " ".join(info)[1:]
